681be021217b133d093a2537d1aabeedd8d14064
BlackNoir5 <103233073+BlackNoir5@users.noreply.github.com>
2024-10-14 11:19:09 +0900

    start project

---
+++ src/main/java/com/example/LightON_Board_Prototype/LightOnBoardPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.example.LightON_Board_Prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightOnBoardPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightOnBoardPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LightON_Board_Prototype/LightOnBoardPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LightON_Board_Prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightOnBoardPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
